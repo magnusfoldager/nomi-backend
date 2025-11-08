36c43f0c67f763bc604c7d047ce4a742ca394fc2
Jonathan Bangert <jonathan@bangert.dk>
2025-11-08 14:20:43 +0100

    fix mcp env name

--- openai.ts
+++ openai.ts
@@ -3,7 +3,7 @@ import OpenAI from "openai";
 const ACI_MCP: OpenAI.Responses.Tool = {
     type: "mcp",
     require_approval: "never",
-    server_url: process.env["MCP_SERVER_URL"],
+    server_url: process.env["ACI_MCP_URL"],
     server_label: "ACTIONS",
 }
 
